cb9017c79fc1e01401f93eec619ed1f8636d6642
Yagouus <yagofontenla95@gmail.com>
2016-10-20 21:11:48 +0200

    Comments function added

--- anaLexico.c
+++ anaLexico.c
@@ -5,18 +5,19 @@
 #include "sistEntrada.h"
 #include "anaLexico.h"
 
+//Variables
+char c;
+int e;
+compLex* comp;
+
 compLex* sigCompLex() {
 
-    int f = 1;
-
-    compLex* comp;
+    //Inicializamos valores
     comp = (compLex *) malloc(sizeof (compLex));
     comp->string = (char *) malloc(16);
-
-    int e = 1;
-
-    char c;
+    e = 1;
     c = sigCaracter();
+
     while (c) {
 
         //printf("%c", c);
@@ -31,7 +32,7 @@ compLex* sigCompLex() {
                     e = 3;
                 } else if (c == ' ' || c == '\n') {
                     c = sigCaracter();
-                } else if (c == '.' || c == ';' || c == '(' || c == ')' || c == '[' || c == ']' || c == ',' || c == '=' || c == '<' || c == '>' || c == '*' || c == '-') {
+                } else if (c == '.' || c == ';' || c == '(' || c == ')' || c == '[' || c == ']' || c == ',' || c == '=' || c == '<' || c == '>' || c == '*' || c == '-' || c == '{' || c == '}') {
                     comp->string[strlen(comp->string)] = c;
                     c = sigCaracter();
                     e = 0;
@@ -66,16 +67,7 @@ compLex* sigCompLex() {
 
                 //Comentarios
             case 4:
-                c = sigCaracter();
-                if (c == '/') {
-                    while(c != '\n'){
-                        c = sigCaracter();
-                    }
-                    c = sigCaracter();
-                    e = 1;
-                }else{
-                    e = 0;
-                }
+                comentarios();
                 break;
 
                 //Estado de aceptacion
@@ -92,4 +84,48 @@ compLex* sigCompLex() {
     }
 
 
+}
+
+//Funcion para reconocer comentarios
+void comentarios() {
+    c = sigCaracter();
+
+    //Comentarios de una linea
+    if (c == '/') {
+        while (c != '\n') {
+            c = sigCaracter();
+        }
+        c = sigCaracter();
+        e = 1;
+
+        //Comentarios de bloque * o +
+    } else if (c == '*' || c == '+') {
+        int x = 1, l = 0;
+        char t = c;
+        while (x != 0) {
+            c = sigCaracter();
+            if (c == t) { //Si encontramos un * o + comprobamos si se esta cerrando el comentario
+                c = sigCaracter();
+                if (c == '/') { //Si se cierra comprobamos que sea el nivel de anidamiento 0
+                    if (l == 0) {
+                        e = 1;
+                        c = sigCaracter();
+                        x = 0;
+                    } else { //Si no es el 0 reducimos un nivel
+                        l--;
+                    }
+
+                }
+                //Si encontramos /+ se anida un comentario
+            } else if (c == '/') {
+                c = sigCaracter();
+                if (c == '+') {
+                    l++; //Aumentamos nivel de anidamiento
+                }
+            }
+        }
+
+    } else {
+        e = 0;
+    }
 }
